89a60d539a7efac266fdbf767397750bb1b34a2d
Ian Ledig <iledig@unistra.fr>
2021-02-09 15:54:28 +0100

    Adding ArrayListTest

--- program/src/com/test/Main.java
+++ program/src/com/test/Main.java
@@ -1,8 +1,23 @@
 package com.test;
 
+import com.test.array.ArrayListTest;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayListTest list = new ArrayListTest();
 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println(list);
+
+        list.insertValue(20, 2);
+
+        System.out.println(list);
     }
 }
--- program/src/com/test/array/ArrayListTest.java
+++ program/src/com/test/array/ArrayListTest.java
@@ -5,20 +5,24 @@ import com.test.ElementsManagement;
 import java.util.ArrayList;
 
 
-public abstract class ArrayListTest extends ArrayList implements ElementsManagement {
+public class ArrayListTest extends ArrayList implements ElementsManagement {
 
     @Override
     public void insertValue(int element, int position) {
-
+        add(get(size() - 1));
+        for(int i = size() - 1; i > position; i--){
+            set(i, get(i - 1));
+        }
+        set(position, element);
     }
 
     @Override
     public void removeValue(int position) {
-
+        remove(position);
     }
 
     @Override
     public int getValue(int position) {
-        return 0;
+        return (int) get(position);
     }
 }
