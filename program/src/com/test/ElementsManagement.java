5beb73a032d31aaa945043a78495eeea367619e5
Ian Ledig <iledig@unistra.fr>
2021-02-09 15:17:46 +0100

    Adding interface

---
+++ program/src/com/test/ElementsManagement.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public interface ElementsManagement {
+
+    void insertValue(int element, int position);
+
+    void removeValue(int position);
+
+    int getValue(int position);
+}
---
+++ program/src/com/test/array/ArrayListTest.java
@@ -0,0 +1,24 @@
+package com.test.array;
+
+import com.test.ElementsManagement;
+
+import java.util.ArrayList;
+
+
+public abstract class ArrayListTest extends ArrayList implements ElementsManagement {
+
+    @Override
+    public void insertValue(int element, int position) {
+
+    }
+
+    @Override
+    public void removeValue(int position) {
+
+    }
+
+    @Override
+    public int getValue(int position) {
+        return 0;
+    }
+}
