f163be7b4c5388bc6835af24ed334f2249dd70d3
Ian Ledig <iledig@unistra.fr>
2021-02-18 15:01:11 +0100

    Editing main

--- program/src/com/test/Main.java
+++ program/src/com/test/Main.java
@@ -1,23 +1,50 @@
 package com.test;
 
 import com.test.array.ArrayListTest;
+import com.test.array.LinkedListTest;
+import com.test.array.VectorTest;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayListTest list = new ArrayListTest();
+        if(args.length < 3){
+            System.out.println("Usage : java Main <0/1/2> <insert/remove/get size");
+            System.exit(1);
+        }
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
+        int size = Integer.parseInt(args[2]);
 
-        System.out.println(list);
+        if(size > 0){
+            ElementsManagement structure = switch (Integer.parseInt(args[0])) {
+                case 0 -> new ArrayListTest();
+                case 1 -> new LinkedListTest();
+                default -> new VectorTest();
+            };
 
-        list.insertValue(20, 2);
+            switch (args[1]) {
+                case "insert" -> insert(size, structure);
+                case "remove" -> remove(size, structure);
+                case "get" -> get(size, structure);
+            }
+        }
+        else{
+            System.out.println("Please select a larger value");
+            System.exit(1);
+        }
+    }
 
-        System.out.println(list);
+    public static void insert(int size, ElementsManagement structure){
+        for (int i = 0; i < size; i++)
+            structure.insertValue(i, i);
+    }
+
+    public static void remove(int size, ElementsManagement structure){
+        insert(size, structure);
+        structure.removeValue(0);
+    }
+
+    public static void get(int size, ElementsManagement structure){
+        insert(size, structure);
+        structure.getValue(0);
     }
 }
